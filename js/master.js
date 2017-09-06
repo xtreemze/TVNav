72c675cff0124f3b7f4ab81513c50f76cc9c3893
Carlos Velasco <carlos@2activedesign.com>
2017-09-06 16:14:21 +0200

    init

---
+++ js/master.js
@@ -0,0 +1,9 @@
+const channel = function(name, link) {
+  this.name = name;
+  this.link = link;
+};
+const tn5 = new channel(
+  "TN5",
+  "https://trinity-lh.akamaihd.net/i/TelevicentroLive2_d@508538/segment798165_800_av-p.ts"
+);
+console.log(tn5);

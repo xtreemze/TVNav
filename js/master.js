32ef4d67d888c7cb3897d418531a51cc20cb4f46
Carlos Velasco <carlos@2activedesign.com>
2017-09-08 22:50:05 +0200

    patio

--- js/master.js
+++ js/master.js
@@ -3,6 +3,15 @@ import fscreen from "fscreen";
 const videoSection = document.getElementById("videoSection");
 const channelSection = document.getElementById("channelSection");
 
+/**
+ * Channel Factory
+ * @param {string} name 
+ * @param {string} shortName 
+ * @param {string} link 
+ * @param {string} type 
+ * @param {string} video 
+ * @param {string} data 
+ */
 const channel = function(name, shortName, link, type, video, data) {
   this.name = name;
   this.shortName = shortName;
@@ -139,7 +148,8 @@ window.rprog = new channel(
 // Donate
 channelSection.innerHTML += `<div class="individualChannel" id="donate"><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="NWT2PUFW6NFLC">
+<input type="hidden" name="hosted_but
+ton_id" value="NWT2PUFW6NFLC">
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form></div>`;

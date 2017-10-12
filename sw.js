ea84581d601802c6c56d596c55b6bf9ed193e123
Carlos Velasco <carlos@2activedesign.com>
2017-10-12 17:03:13 +0200

    offline support

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ var __wpo = {
     "main": [
       "./",
       "./css/font/VideoJS.eot",
-      "./img/bars.svg",
       "./Images/52a2a529-8f1b-d5b3-ce83-a41878e54a71.webPlatform.png",
+      "./img/bars.svg",
       "./bundle.js",
       "./css/video-js.min.css",
       "./js/video.min.js",
@@ -25,13 +25,13 @@ var __wpo = {
   "hashesMap": {
     "a0ab7b93a70e8ecceaa4fcc0b4ef95d77f4b5665": "./",
     "f494a1aec8fc46fb7ad59b441a088783a7a2eef9": "./css/font/VideoJS.eot",
-    "89c96e7bf126dfa6dbf54a15ba98db502a450bd5": "./img/bars.svg",
     "47698cfc3b377868469932fec8355f5dd5e38485": "./Images/52a2a529-8f1b-d5b3-ce83-a41878e54a71.webPlatform.png",
+    "89c96e7bf126dfa6dbf54a15ba98db502a450bd5": "./img/bars.svg",
     "7177c680f4a81916fb94b93b429358bf30ce1c81": "./bundle.js"
   },
   "strategy": "all",
   "responseStrategy": "network-first",
-  "version": "2017-10-12 16:57:11",
+  "version": "2017-10-12 16:58:36",
   "name": "webpack-offline",
   "pluginVersion": "4.8.4",
   "relativePaths": true

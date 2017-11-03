bb695de0ea568a72b5edf15429a07d912caa92e0
Carlos Velasco <carlos@2activedesign.com>
2017-11-03 22:09:46 +0100

    clean

--- entry.js
+++ entry.js
@@ -1,10 +1,4 @@
 require("./indexB.html");
-// require("./css/materialdesignicons.css");
-// require("./css/video-js.min.css");
 require("./node_modules/video.js/dist/video-js.css");
 require("./css/master.css");
-// require("./js/video.min.js");
-// require("./js/videojs-contrib-hls.min");
 require("./js/master.js");
-// import * as OfflinePluginRuntime from "offline-plugin/runtime";
-// OfflinePluginRuntime.install();
